3f4c8fe03b1585dbdfc47b726db44846c1a08033
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2016-07-13 11:03:34 -0400

    Revert "Changed default behavior of DataFrameBuilder to add a gene column"

--- isovar/dataframe_builder.py
+++ isovar/dataframe_builder.py
@@ -21,10 +21,6 @@ import pandas as pd
 
 VALID_ELEMENT_TYPES = integer_types + (text_type, binary_type, float, bool)
 
-# values of these types are automatically converted to their size or length
-# unless some other conversion function is provided
-COLLECTION_TYPES = (tuple, list, set, frozenset)
-
 class DataFrameBuilder(object):
     """
     Helper class for constructing a DataFrame which always has fields
@@ -37,7 +33,6 @@ class DataFrameBuilder(object):
             exclude=set([]),
             converters={},
             rename_dict={},
-            extra_column_fns={},
             variant_columns=True,
             convert_collections_to_size=True):
         """
@@ -60,15 +55,8 @@ class DataFrameBuilder(object):
             Dictionary mapping element_class field names to desired column names
             in the produced DataFrame.
 
-        extra_column_fns : dict
-            Dictionary mapping column names to functions which take a variant
-            and element (such as an AlleleRead instance) and return a single
-            value for each row.
-
         variant_columns : bool
             If True, then add four columns for fields of a Variant: chr/pos/ref/alt
-            along with a "gene" column indicating which gene name(s) the variant
-            overlaps.
 
         convert_collections_to_size : bool
             If a value is a built-in collection (list, tuple, or set) then
@@ -105,17 +93,12 @@ class DataFrameBuilder(object):
                 ("pos", []),
                 ("ref", []),
                 ("alt", []),
-                ("gene", []),
             ]
         else:
             columns_list = []
 
         for name in self.renamed_field_names:
             columns_list.append((name, []))
-
-        self.extra_column_fns = extra_column_fns
-        for column_name in self.extra_column_fns:
-            columns_list.append((column_name, []))
 
         self.columns_dict = OrderedDict(columns_list)
 
@@ -126,7 +109,6 @@ class DataFrameBuilder(object):
             self.columns_dict["pos"].append(variant.original_start)
             self.columns_dict["ref"].append(variant.original_ref)
             self.columns_dict["alt"].append(variant.original_alt)
-            self.columns_dict["gene"].append(";".join(variant.gene_names))
         else:
             assert variant is None
 
@@ -139,7 +121,7 @@ class DataFrameBuilder(object):
                 fn = self.converters[name]
                 value = fn(value)
 
-            if isinstance(value, COLLECTION_TYPES) and self.convert_collections_to_size:
+            if isinstance(value, (tuple, list, set)) and self.convert_collections_to_size:
                 value = len(value)
             elif not isinstance(value, VALID_ELEMENT_TYPES):
                 raise ValueError(
@@ -151,30 +133,11 @@ class DataFrameBuilder(object):
                 name = self.rename_dict[name]
             self.columns_dict[name].append(value)
 
-        for column_name, fn in self.extra_column_fns.items():
-            self.columns_dict[column_name].append(fn(variant, element))
-
     def add_many(self, variant, elements):
         for element in elements:
             self.add(variant, element)
 
-    def _check_column_lengths(self):
-        """
-        Make sure columns are of the same length or else DataFrame construction
-        will fail.
-        """
-        column_lengths_dict = {
-            name: len(xs)
-            for (name, xs)
-            in self.columns_dict.items()
-        }
-        unique_column_lengths = set(column_lengths_dict.values())
-        if len(unique_column_lengths) != 1:
-            raise ValueError(
-                "Mismatch between lengths of columns: %s" % (column_lengths_dict,))
-
     def to_dataframe(self):
-        self._check_column_lengths()
         return pd.DataFrame(self.columns_dict)
 
 def dataframe_from_generator(element_class, variant_and_elements_generator, **kwargs):
