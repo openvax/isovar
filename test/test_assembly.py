bb757cbe04d3c3d5d3e1063ec6fdd1c79fa39994
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2016-11-04 16:48:52 -0400

    assembly unit test with mock reads

--- test/test_assembly.py
+++ test/test_assembly.py
@@ -59,11 +59,46 @@ def test_assemble_transcript_fragments_snv():
         assert len(s) > max_read_length, \
             "Expected assembled sequences to be longer than read length (%d)" % (
                 max_read_length,)
-"""
-def test_assemble_simple_sequences():
-    AlleleRead
-    VariantSequence
-"""
 
-if __name__ == "__main__":
-    test_assemble_transcript_fragments_snv()
+def test_assembly_of_simple_sequence_from_mock_reads():
+    #
+    # Read sequences:
+    #    AAAAA|CC|TTTTT
+    #    AAAAA|CC|TTTTT
+    #   GAAAAA|CC|TTTTTG
+    #     AAAA|CC|TTTT
+    reads = [
+        # two identical reads with sequence AAAAA|CC|TTTTT
+        AlleleRead(prefix="A" * 5, allele="CC", suffix="T" * 5, name="dup1"),
+        AlleleRead(prefix="A" * 5, allele="CC", suffix="T" * 5, name="dup2"),
+        # longer sequence GAAAAA|CC|TTTTTG
+        AlleleRead(prefix="G" + "A" * 5, allele="CC", suffix="T" * 5 + "G", name="longer"),
+        # shorter sequence AAAA|CC|TTTT
+        AlleleRead(prefix="A" * 4, allele="CC", suffix="T" * 4, name="shorter"),
+    ]
+    expected_variant_sequence = VariantSequence(
+        prefix="G" + "A" * 5, alt="CC", suffix="T" * 5 + "G", reads=reads)
+    initial_variant_sequences = initial_variant_sequences_from_reads(reads)
+    # expecting one fewer sequence than reads since two of the reads are
+    # duplicates
+    eq_(len(initial_variant_sequences), len(reads) - 1)
+
+    assembled_variant_sequences = iterative_overlap_assembly(
+        initial_variant_sequences,
+        min_overlap_size=1)
+
+    # since no reads contradict each other then we should get back a single
+    # assembled sequence
+    eq_(len(assembled_variant_sequences),
+        1,
+        "Unexpected number of variant sequences: %s" % (assembled_variant_sequences,))
+    assembled_variant_sequence = assembled_variant_sequences[0]
+    eq_(assembled_variant_sequence, expected_variant_sequence)
+
+    eq_(len(assembled_variant_sequence.reads), len(reads))
+
+    eq_(assembled_variant_sequence.min_coverage(), 1)
+    # 2 bases with 1/4 reads, 2 bases with 3/4 reads, remaining 10 bases with
+    # all 4/4 reads
+    expected_mean_coverage = (2 * 1 + 2 * 3 + 10 * 4) / 14
+    eq_(assembled_variant_sequence.mean_coverage(), expected_mean_coverage)
