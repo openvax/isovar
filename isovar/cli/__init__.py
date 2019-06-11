e07ae4dfebed2a77152fae4d333bd398b30a03a9
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2019-06-11 12:27:12 -0400

    import new CLI helpers in __init__

--- isovar/cli/__init__.py
+++ isovar/cli/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016. Mount Sinai School of Medicine
+# Copyright (c) 2019. Mount Sinai School of Medicine
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,4 +15,9 @@
 
 from __future__ import print_function, division, absolute_import
 
-from .main_args import
+from .main_args import make_isovar_arg_parser, run_isovar_from_parsed_args
+
+__all__ = [
+    "make_isovar_arg_parser",
+    "run_isovar_from_parsed_args"
+]
