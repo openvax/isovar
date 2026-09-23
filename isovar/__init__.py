adb298f8800e840f2b20f5b0303d6d80507f5d7e
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2026-09-23 07:49:49 -0400

    Preserve seed-spanning SV RNA paths alongside regional assembly

--- isovar/__init__.py
+++ isovar/__init__.py
@@ -10,7 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.21.9"
+__version__ = "1.21.10"
 
 
 from .allele_read import AlleleRead
